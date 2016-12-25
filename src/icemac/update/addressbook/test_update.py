e18bbdc1f37d4001a773f53d7ee29913c7922c70
Michael Howitz <mh@gocept.com>
2016-12-25 15:21:50 +0100

    Implement function to get the download url.

---
+++ src/icemac/update/addressbook/test_update.py
@@ -0,0 +1,36 @@
+from update import download_url
+import mock
+import pkg_resources
+import pytest
+
+
+def test_update__download_url__1():
+    """It returns the URL to download the specified address book version."""
+    with mock.patch('urllib.urlopen') as urlopen:
+        urlopen.return_value = pkg_resources.resource_stream(
+            'icemac.update.addressbook', 'fixtures/icemac.addressbook.json')
+        assert (
+            'https://pypi.python.org/packages/dd/01/'
+            '36f28ce3db10431ec21245233e6562fc609d8301003b981b28fb3aedcf67/'
+            'icemac.addressbook-1.10.5.zip' == download_url('1.10.5'))
+
+
+def test_update__download_url__2():
+    """It raises a ValueError if the specified version does not exist."""
+    with mock.patch('urllib.urlopen') as urlopen:
+        urlopen.return_value = pkg_resources.resource_stream(
+            'icemac.update.addressbook', 'fixtures/icemac.addressbook.json')
+        with pytest.raises(ValueError) as err:
+            download_url('24.11')
+        assert "Release '24.11' does not exist." == str(err.value)
+
+
+def test_update__download_url__3():
+    """It raises a ValueError if the specified version has not sdist."""
+    with mock.patch('urllib.urlopen') as urlopen:
+        urlopen.return_value = pkg_resources.resource_stream(
+            'icemac.update.addressbook', 'fixtures/icemac.addressbook.json')
+        with pytest.raises(ValueError) as err:
+            download_url('1.1.1')
+        assert ("Release '1.1.1' does not have an sdist release." ==
+                str(err.value))
--- src/icemac/update/addressbook/update.py
+++ src/icemac/update/addressbook/update.py
@@ -1,2 +1,22 @@
-# def main():
-#     print "update-main"
+import json
+import urllib
+
+
+PYPI_JSON_URL = (
+    'https://pypi.python.org/simple/icemac.addressbook/json')
+
+
+def download_url(version):
+    """Get the download_url."""
+    with urllib.urlopen(PYPI_JSON_URL.format(version)) as f:
+        data = json.load(f)
+    releases = data['releases']
+    try:
+        packages = releases[version]
+    except KeyError:
+        raise ValueError('Release {!r} does not exist.'.format(version))
+    for package in packages:
+        if package['packagetype'] == 'sdist':
+            return package['url']
+    raise ValueError(
+        'Release {!r} does not have an sdist release.'.format(version))
