92f820f74850034632edda8296639c5658dfb431
Michael Howitz <mh@gocept.com>
2016-12-29 11:04:19 +0100

    Keep the path so the html coverage dir is not created in the temp dir.

--- src/icemac/update/addressbook/conftest.py
+++ src/icemac/update/addressbook/conftest.py
@@ -1,8 +1,11 @@
+import os
 import pytest
 
 
 @pytest.fixture('function')
 def basedir(tmpdir):
     """Create a base directory for the tests and chdir to it."""
+    cwd = os.getcwd()
     tmpdir.chdir()
     yield tmpdir
+    os.chdir(cwd)
