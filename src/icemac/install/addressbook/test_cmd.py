c3752f5d5851c1db42cff6797c3f0e738e1e5b6c
Michael Howitz <mh@gocept.com>
2017-01-06 11:19:59 +0100

    Fix ordering problem

--- src/icemac/install/addressbook/test_cmd.py
+++ src/icemac/install/addressbook/test_cmd.py
@@ -6,7 +6,8 @@ def test_cmd__call_cmd__1(basedir):
     """It calls a command and return nothing."""
     basedir.mkdir('test')
     assert None is call_cmd('tar', '-cjf', 'test.tar.bz2', 'test')
-    assert ['test', 'test.tar.bz2'] == [x.basename for x in basedir.listdir()]
+    assert ({'test', 'test.tar.bz2'} ==
+            set([x.basename for x in basedir.listdir()]))
 
 
 def test_cmd__call_cmd__2(basedir):
